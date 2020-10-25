a98e77b3624d7dbbdad00f14571a1c6c997cc77b
Hashemi Rafsan <rafsanhashemi@gmail.com>
2020-10-25 21:08:46 +0600

    add build

---
+++ build/precache-manifest.5974a586cc32e4df726aa8bbfb9abfc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfed418dd48a09e919b3729833e1e6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e282bfdd02b95bf021c",
+    "url": "/static/css/2.d782ba9a.chunk.css"
+  },
+  {
+    "revision": "45bcc07f3b29fc9cbd3f",
+    "url": "/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "8e282bfdd02b95bf021c",
+    "url": "/static/js/2.48f1c7cc.chunk.js"
+  },
+  {
+    "revision": "69c5fecb15033eb3fd434d9d1055fed1",
+    "url": "/static/js/2.48f1c7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bcc07f3b29fc9cbd3f",
+    "url": "/static/js/main.ea190e69.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
